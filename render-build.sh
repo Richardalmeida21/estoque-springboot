69acd84e0b9f3ebd5ee3fcc47da311e08d0e55c9
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-02-20 16:06:18 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Instalar Java e configurar JAVA_HOME
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+export PATH=$JAVA_HOME/bin:$PATH
+
 # Instalar Maven manualmente
 curl -sL https://archive.apache.org/dist/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz | tar xz
 export PATH=$PWD/apache-maven-3.8.8/bin:$PATH
