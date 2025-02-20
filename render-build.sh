d65644362a11d02dcf92ba3ec55a861de344ee47
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-02-20 16:08:05 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# Instalar Java e configurar JAVA_HOME
-export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
+# Instalar Java manualmente (caso o Render não tenha)
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Instalar Maven manualmente
 curl -sL https://archive.apache.org/dist/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz | tar xz
 export PATH=$PWD/apache-maven-3.8.8/bin:$PATH
+
+# Mostrar versões para depuração
+java -version
+mvn -version
 
 # Compilar o projeto
 mvn clean package
